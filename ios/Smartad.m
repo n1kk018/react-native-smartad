5190ca448c14c7041d040b984d4bad134dfb5d20
n1kk018 <nicolastorero@gmail.com>
2020-06-28 14:24:29 +0200

    fix

--- ios/Smartad.m
+++ ios/Smartad.m
@@ -114,10 +114,10 @@ RCT_EXPORT_METHOD(reset)
     [self sendEventWithName:kSmartAdInterstitialAdClicked body:nil];
 }
 
-- (void)interstitialManager:(SASInterstitialManager *)manager didSendVideoEvent: (SASVideoEvent *)videoEvent {
+/* - (void)interstitialManager:(SASInterstitialManager *)manager didSendVideoEvent: (SASVideoEvent *)videoEvent {
     NSLog(@"Interstitial did send video event: %li", (long)videoEvent);
     [self sendEventWithName:kSmartAdInterstitialAdVideoEvent body:nil];
-}
+} */
 
 
 /* - (void)rewardedVideoManager:(SASRewardedVideoManager *)manager willPresentModalViewFromViewController: (UIViewController *)controller {

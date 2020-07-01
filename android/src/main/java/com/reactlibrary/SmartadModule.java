6483e3568f947325f75e4d82d64e37aecefc4431
nikko <nicolastorero@gmail.com>
2020-07-01 12:20:38 +0200

    test

--- android/src/main/java/com/reactlibrary/SmartadModule.java
+++ android/src/main/java/com/reactlibrary/SmartadModule.java
@@ -69,12 +69,12 @@ public class SmartadModule extends ReactContextBaseJavaModule {
             @Override
             public void run() {
                 SASConfiguration.getSharedInstance().configure(reactContext, SITE_ID, "https://mobile.smartadserver.com");
-                mInterstitialPlacement = new SASAdPlacement(SITE_ID, PAGE_ID, FORMAT_ID, TARGET);
-                if (BuildConfig.DEBUG) {
+                //mInterstitialPlacement = new SASAdPlacement(SITE_ID, PAGE_ID, FORMAT_ID, TARGET);
+                //if (BuildConfig.DEBUG) {
                     mInterstitialPlacement = SASAdPlacement.MRAID_INTERSTITIAL_TEST_PLACEMENT;
                     //mInterstitialPlacement = SASAdPlacement.VIDEO_INTERSTITIAL_TEST_PLACEMENT;
                     //mInterstitialPlacement = SASAdPlacement.VIDEO_360_INTERSTITIAL_TEST_PLACEMENT;
-                }
+                //}
                 mInterstitialManager = new SASInterstitialManager(reactContext, mInterstitialPlacement);
                 initInterstitialListener();
                 mInterstitialManager.setInterstitialListener(mInterstitialListener);
